39ecf6a751c52630ca822a933d51f3e3e27b2672
truongnx <kris@DongTD-D8>
2024-04-01 17:13:21 +0700

    init commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		_, err := ec2.NewVpc(ctx, "main", &ec2.VpcArgs{
+			CidrBlock: pulumi.String("10.0.0.0/16"),
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
